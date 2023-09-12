551df6e4eca99bda6e2b889baabaff8bfe10b325
DeepakLenka-space <127427297+DeepakLenka-space@users.noreply.github.com>
2023-09-12 17:18:22 +0530

    Add files via upload

---
+++ rockpapergame/word.py
@@ -0,0 +1,43 @@
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = input("Choose rock, paper, or scissors: ").lower()
+        if user_choice in ["rock", "paper", "scissors"]:
+            return user_choice
+        else:
+            print("Invalid choice. Please enter rock, paper, or scissors.")
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    print("Welcome to Rock, Paper, Scissors!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+
+        print(f"You chose {user_choice}.")
+        print(f"The computer chose {computer_choice}.")
+
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            break
+
+if __name__ == "__main__":
+    play_game()
